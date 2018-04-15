b83137a9ab44220e536c9cfc185636be76b3f6e0
alere <alere@ALEJANDRA-MENA>
2018-04-14 21:16:31 -0600

    Prueba para guía 1

---
+++ src/proyectosms/ProyectoSMS.java
@@ -0,0 +1,18 @@
+
+package proyectosms;
+
+/**
+ * Nombre de la clase: ProyectoSMS
+ * Fecha: 14/04/18
+ * Version: 1.0
+ * CopyRight: ITCA
+ * @author alere
+ */
+public class ProyectoSMS {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
